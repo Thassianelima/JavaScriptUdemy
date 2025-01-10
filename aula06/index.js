a36395d048c9fd66134716ad2aa66ceac9b2cf25
Thassiane08 <thassiane321@hotmail.com>
2025-01-10 20:45:12 -0300

    Update index.js

---
+++ aula06/index.js
@@ -0,0 +1,13 @@
+//nao podemos criar variaveis com palavras reservadas
+//variaveis precisam ter nomes significatios
+//nao podemos começar o nome de uma variavel com numero
+// nao podem conter espaços ou traços
+// ultilizamos camelCase
+//Case-sensitive
+// nao podemos redeclarar variaveis com let
+//nao ultilize var, ultilize let
+
+let nomeCliente = 'Thassiane lima';
+nomeCliente = 'Thassiane'
+
+console.log(nomeCliente)
