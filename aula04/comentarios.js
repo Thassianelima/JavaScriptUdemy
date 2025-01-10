96a4a132ec90604fed9ca962a187f473c9624f48
Thassiane08 <thassiane321@hotmail.com>
2025-01-10 16:30:03 -0300

    update

---
+++ aula04/comentarios.js
@@ -0,0 +1,11 @@
+// comentario
+console.log('olá') //outro comentario aqui
+console.log('linha1')
+//outro comentario
+
+/*outro tipo de comentario*/
+
+//console.log('linha2')
+console.log('linha3')
+console.log('linha4')
+//outro comentario que vai ser ignorado
