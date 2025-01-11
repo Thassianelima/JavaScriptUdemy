6d10bc442288ec4d9f591c7587e2597749056aae
Thassiane08 <thassiane321@hotmail.com>
2025-01-10 21:03:37 -0300

    Create index.js

---
+++ aula07/index.js
@@ -0,0 +1,18 @@
+//nao podemos criar variaveis com palavras reservadas
+//variaveis precisam ter nomes significatios
+//nao podemos começar o nome de uma variavel com numero
+// nao podem conter espaços ou traços
+// ultilizamos camelCase
+//Case-sensitive
+// nao podemos redeclarar variaveis com let
+//nao ultilize var, ultilize let
+
+//string = text number = numero
+const primeiroNumero = 5
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+let resultadoTriplicado = resultado * 3
+resultadoTriplicado = resultadoTriplicado + 5
+
+console.log(primeiroNumero + segundoNumero)
